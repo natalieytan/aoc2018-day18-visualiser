5ee5b6d3e097442100efa3b1d6597df36bf78992
Natalie Tan <natalieytan@gmail.com>
2018-12-21 00:04:41 +1100

    Working Grid, InputForm, GridCustomiser

---
+++ src/components/Grid.js
@@ -0,0 +1,180 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0,
+      width: 0,
+      grid: null,
+      trees: [],
+      lumber: [],
+      ground: [],
+      counter: 0,
+      running: false,
+      looper: null,
+    }
+    this.pause = this.pause.bind(this)
+    this.play = this.play.bind(this)
+    this.everyMinute = this.everyMinute.bind(this)
+    this.resetInput = this.resetInput.bind(this)
+  }
+
+  componentDidMount() {
+    const grid = this.props.input.split(/\n/).map(row => row.split(''))
+    const trees = []
+    const lumber = []
+    const ground = []
+    for (const [y, row] of grid.entries()) {
+      for (const [x, symbol] of row.entries()) {
+        if (symbol === "|") {
+          trees.push({x, y})
+        }
+        else if (symbol === "#") {
+          lumber.push({x, y})
+        }
+        else if (symbol === ".") {
+          ground.push({x, y})
+        }
+      }
+    }
+    this.setState({counter: 0, grid, lumber, ground, trees, running: true, height: grid.length, width: grid[0].length})
+    const looper = setInterval(this.everyMinute, 80) 
+    this.setState({looper})
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.looper)
+  }
+
+
+  resetInput(){
+    clearInterval(this.state.looper)
+    this.setState({counter: 0, input: null, height: 0, width: 0, grid: null, trees: [], lumber: [], ground: [], running: false})
+    this.props.handleInputChange(null)
+  }
+
+  pause(){
+    clearInterval(this.state.looper)
+    this.setState({running: false})
+  }
+
+  play(){
+    const looper = setInterval(this.everyMinute, 80) 
+    this.setState({looper, running: true})
+  }
+
+  has_surrounding_trees(surrounds, n, grid) {
+    let count = 0
+    for (const coord of surrounds) {
+      if (grid[coord.y][coord.x] === "|") {count += 1}
+      if (count >= n) { return true }
+    }
+    return false
+  }
+
+  has_surrounding_lumber(surrounds, n, grid) {
+    let count = 0    
+    for (const coord of surrounds) {
+      if (grid[coord.y][coord.x] === "#") {count += 1}
+      if (count >= n) { return true }
+    }
+    return false
+  }
+
+  get_surrounding(coords) {
+    const surrounds = []
+    const x = coords.x
+    const y = coords.y
+    if (y > 0) { surrounds.push({y: y - 1, x: x}) }
+    if (y > 0 && x < (this.state.width - 1)) { surrounds.push({y: y - 1, x: x + 1}) }
+    if (x < (this.state.width - 1)) { surrounds.push({y, x: x + 1}) }
+    if (y < (this.state.height - 1) && x < (this.state.width - 1)) { surrounds.push({y: y + 1, x: x + 1}) }
+    if (y < (this.state.height - 1)) { surrounds.push({y: y + 1, x}) }
+    if (y <  (this.state.height - 1) && x > 0) { surrounds.push({y: y + 1, x: x - 1}) }
+    if (x > 0) { surrounds.push({y, x: x - 1}) }
+    if (y > 0 && x > 0) { surrounds.push({y: y - 1, x: x - 1}) }
+    return surrounds
+  }
+
+  everyMinute() {
+    this.setState((prevState, _) => {
+      const new_grid = new Array(prevState.height).fill(null).map(x => new Array(prevState.width).fill('.'))
+      const new_trees = []
+      const new_lumber =[]
+      const new_ground = []
+      for (const coord of prevState.ground) {
+        const surrounds = this.get_surrounding(coord)
+        if (this.has_surrounding_trees(surrounds, 3, prevState.grid)) {
+          new_trees.push(coord)
+          new_grid[coord.y][coord.x] = '|'
+        } else {
+          new_ground.push(coord)
+        }
+      }
+      for (const coord of prevState.trees) {
+        const surrounds = this.get_surrounding(coord)
+        if (this.has_surrounding_lumber(surrounds, 3, prevState.grid)) {
+          new_lumber.push(coord)
+          new_grid[coord.y][coord.x] = '#'
+        } else {
+          new_trees.push(coord)
+          new_grid[coord.y][coord.x] = '|'
+        }
+      }
+  
+     for (const coord of prevState.lumber) {
+      const surrounds = this.get_surrounding(coord, prevState.grid)
+       if (this.has_surrounding_lumber(surrounds, 1, prevState.grid) && this.has_surrounding_trees(surrounds, 1, prevState.grid)) {
+         new_lumber.push(coord)
+         new_grid[coord.y][coord.x] = "#"
+       } else {
+         new_ground.push(coord)
+       }
+     }
+
+      return { counter: prevState.counter + 1,
+        trees: new_trees,
+        lumber: new_lumber,
+        ground: new_ground,
+        grid: new_grid }
+    })
+  }
+
+  
+  render() {
+    let playPause;
+    if (this.state.running) {
+      playPause = <button onClick={this.pause}> Pause </button>
+    } else {
+      playPause = <button onClick={this.play}> Play </button>
+    }
+
+    let items;
+    if (this.state.grid) {
+      if (this.props.display === "display-text") {
+        items = this.state.grid.map(
+          (row, j)  => <tr key={j}> {row.map( (e, i) => <td key={i} style={{color: this.props.customText[e]}}>{e}</td> )}</tr>)
+      } else if (this.props.display === "display-emoji") {
+        items = this.state.grid.map(
+          (row, j)  => <tr key={j}> {row.map( (e, i) => <td key={i}><span>{this.props.customEmoji[e]}</span></td> )}</tr>)
+      } else if (this.props.display === "display-solid-color") {
+        items = this.state.grid.map(
+          (row, j) => <tr key={j}> {row.map( (e, i) => <td key={i} style={{background: this.props.customColor[e]}}></td> )}</tr>)
+      }
+    }
+    return (
+      <div>
+      <button onClick={this.resetInput}> Reset Input </button>
+        {playPause}
+      <div>{this.state.counter}</div>
+      <table>
+        <tbody>
+        {items}
+        </tbody>
+      </table>
+      </div>
+  )}
+
+}
+export default Grid
--- src/components/GridTypeSelector.js
+++ src/components/GridTypeSelector.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
-import Grid from './components/Grid'
-import InputForm from './components/InputForm'
+import React, { Component } from 'react'
 
-class App extends Component {
+class GridTypeSelector extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: null,
       display: "display-text",
       groundText: "#CCCCCC",
       lumberText: "#333333",
@@ -19,7 +15,6 @@ class App extends Component {
       lumberEmoji : "⛺️",
       treeEmoji:  "🌲"
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleDisplayChange = this.handleDisplayChange.bind(this)
     this.handleCustomChange = this.handleCustomChange.bind(this)
     this.handleEmoji = this.handleEmoji.bind(this)
@@ -29,37 +24,20 @@ class App extends Component {
     this.setState({[event.target.name]: event.target.value})
   }
 
-  handleCustomChange(event) {
-    this.setState({[event.target.name]: event.target.value})
-  }
-
   handleDisplayChange(event) {
     this.setState({display: event.target.value});
   }
 
-  handleInputChange(input) {
-    this.setState({input})
+  handleCustomChange(event) {
+    this.setState({[event.target.name]: event.target.value})
   }
-  
+
+
   render() {
-    let leftSide;
-    if(this.state.input) {
-      leftSide = <Grid input={this.state.input}
-                      handleInputChange = {this.handleInputChange}
-                      display = {this.state.display}
-                      customText = { {"#": this.state.lumberText, "|": this.state.treeText, ".": this.state.groundText} }
-                      customEmoji = { {"#": this.state.lumberEmoji, "|": this.state.treeEmoji, ".": this.state.groundEmoji} }
-                      customColor = { {"#": this.state.lumberColor, "|": this.state.treeColor, ".": this.state.groundColor} }
-                />
-    } else {
-      leftSide = <InputForm onInputChange={this.handleInputChange}/>  
-    }
-
-
     let selection;
     if (this.state.display === "display-text") {
       selection = (
-      <form className="custom-select">
+      <form>
       <label htmlFor="ground-text">Ground: </label>
       <input type="color" value={this.state.groundText} id="ground-text" name="groundText" onChange={this.handleCustomChange}/> 
       <label htmlFor="tree-text">Tree: </label>
@@ -70,52 +48,49 @@ class App extends Component {
       )
     } else if (this.state.display === "display-emoji") {
       selection = (
-      <form className="custom-select">
-        <label htmlFor="ground-emoji">Ground: </label>
+      <form>
         <select value={this.state.groundEmoji} id="ground-emoji" name="groundEmoji" onChange={this.handleEmoji}>
-          <option value="〰️">{"〰️"}</option>
-          <option value="➰">{"➰"}</option>
-          <option value="➿">{"➿"}</option>
-          <option value="🌀">{"🌀"}</option>
-          <option value="❗️">{"❗️"}</option>
-          <option value="❌">{"❌"}</option>
-          <option value="💩">{"💩"}</option>
-          <option value="☁️">{"☁️"}</option>
-          <option value="❤️">{"❤️"}</option>
-          <option value="⚠️">{"⚠️"}</option>
+          <option value="〰️">〰️</option>
+          <option value="➰">➰</option>
+          <option value="➿">➿</option>
+          <option value="🌀">🌀</option>
+          <option value="❗️">❗️</option>
+          <option value="❌">❌</option>
+          <option value="💩">💩</option>
+          <option value="☁️">☁️</option>
+          <option value="❤️">❤️</option>
+          <option value="⚠️">⚠️</option>
         </select>
 
-        <label htmlFor="tree-emoji">Tree: </label>
         <select value={this.state.treeEmoji} id="tree-emoji" name="treeEmoji" onChange={this.handleEmoji}>
-          <option value="🌲">{"🌲"}</option>
-          <option value="🎄">{"🎄"}</option>
-          <option value="🌳">{"🌳"}</option>
-          <option value="🌴">{"🌴"}</option>
-          <option value="🌵">{"🌵"}</option>
-          <option value="🌱">{"🌱"}</option>
-          <option value="🍄">{"🍄"}</option>
-          <option value="🌸">{"🌸"}</option>
-          <option value="🌻">{"🌻"}</option>
-          <option value="🦖">{"🦖"}</option>
+          <option value="🌲">🌲</option>
+          <option value="🎄">🎄</option>
+          <option value="🌳">🌳</option>
+          <option value="🌴">🌴</option>
+          <option value="🌵">🌵</option>
+          <option value="🌱">🌱</option>
+          <option value="🍄">🍄</option>
+          <option value="🌸">🌸</option>
+          <option value="🌻">🌻</option>
+          <option value="🦖 ">🦖 </option>
         </select>
 
-        <label htmlFor="lumber-emoji">Lumber: </label>
         <select value={this.state.lumberEmoji} id="lumber-emoji" name="lumberEmoji" onChange={this.handleEmoji}>
-          <option value="⛺️">{"⛺️"}</option>
-          <option value="🏕">{"🏕"}</option>
-          <option value="🚧 ">{"🚧"}</option>
-          <option value="🗿">{"🗿"}</option>
-          <option value="🚀">{"🚀"}</option>
-          <option value="🌱">{"🌱"}</option>
-          <option value="🔥">{"🔥"}</option>
-          <option value="💥">{"💥"}</option>
-          <option value="⭐️">{"⭐️"}</option>
-          <option value="⚡️ ">{"⚡️"} </option>
+          <option value="⛺️">⛺️</option>
+          <option value="🏕">🏕</option>
+          <option value="🚧 ">🚧 </option>
+          <option value="🗿">🗿</option>
+          <option value="🚀">🚀</option>
+          <option value="🌱">🌱</option>
+          <option value="🔥">🔥</option>
+          <option value="💥">💥</option>
+          <option value="⭐️">⭐️</option>
+          <option value="⚡️ ">⚡️ </option>
         </select>
       </form>)
-    } else if (this.state.display === "display-solid-color") {
+    } else if(this.state.display === "display-solid-color") {
       selection = (
-        <form className="custom-select">
+        <form>
         <label htmlFor="ground-text">Ground: </label>
         <input type="color" value={this.state.groundColor} id="ground-color" name="groundColor" onChange={this.handleCustomChange}/> 
         <label htmlFor="tree-color">Tree: </label>
@@ -125,12 +100,9 @@ class App extends Component {
         </form>
         )
     }
-      
+    
     return (
-    <div className="App">
-      <h2>AOC 2018 Day 18 Visualiser</h2>
       <div>
-        <h3>Customise display</h3>
         <form className="display-options">
           <input type="radio" id="display-text" name="display" value="display-text" 
                   checked={this.state.display === 'display-text' } onChange={this.handleDisplayChange} />
@@ -142,18 +114,15 @@ class App extends Component {
 
           <input type="radio" id="display-solid-color" name="display" value="display-solid-color" 
                 checked={this.state.display === 'display-solid-color'} onChange={this.handleDisplayChange} />
-          <label htmlFor="display-solid-color">Colored Squares</label>
+          <label htmlFor="display-solid-color">Solid Color</label>
         </form>
 
         {selection}
         
       </div>
+    )
 
-
-      <div> {leftSide} </div>
-      </div>
-    );
   }
 }
 
-export default App;
+export default GridTypeSelector
