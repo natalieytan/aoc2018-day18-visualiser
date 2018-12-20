fbce54cb710da59834bda2b71157e4ca0f3eecfd
Natalie Tan <natalieytan@gmail.com>
2018-12-21 10:44:59 +1100

    Updates

---
+++ precache-manifest.05ae23eded94a81ec5fd327bb6ac1be7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64cff167f9cb75fb95a6",
+    "url": "/aoc2018-day18-visualiser/static/js/runtime~main.64cff167.js"
+  },
+  {
+    "revision": "691263ac69375486ded0",
+    "url": "/aoc2018-day18-visualiser/static/js/main.691263ac.chunk.js"
+  },
+  {
+    "revision": "27333f6ecb71f92d1a5a",
+    "url": "/aoc2018-day18-visualiser/static/js/1.27333f6e.chunk.js"
+  },
+  {
+    "revision": "691263ac69375486ded0",
+    "url": "/aoc2018-day18-visualiser/static/css/main.21711e77.chunk.css"
+  },
+  {
+    "revision": "a5c5d3ce8fb2e9fa0896bdc726cd795e",
+    "url": "/aoc2018-day18-visualiser/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/aoc2018-day18-visualiser/precache-manifest.05ae23eded94a81ec5fd327bb6ac1be7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/aoc2018-day18-visualiser/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
